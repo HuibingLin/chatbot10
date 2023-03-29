6e750a712eaff3e51155dcc3be21b1e14ee1031c
huibing <13728166372@163.com>
2023-03-29 17:12:46 +0800

    Third commit

--- chatbot.py
+++ chatbot.py
@@ -2,13 +2,15 @@
 Author: huibing 13728166372@163.com
 Date: 2023-03-28 21:09:05
 LastEditors: huibing 13728166372@163.com
-LastEditTime: 2023-03-29 16:16:01
+LastEditTime: 2023-03-29 17:06:20
 FilePath: /chatbot10/chatbot.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 import configparser
 import random
 import pyodbc
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
@@ -22,9 +24,15 @@ azure_sql_database = config.get('AZURE_SQL', 'DATABASE')
 azure_sql_user = config.get('AZURE_SQL', 'USER')
 azure_sql_password = config.get('AZURE_SQL', 'PASSWORD')
 azure_sql_driver = config.get('AZURE_SQL', 'DRIVER')
+spotify_client_id = config.get('SPOTIFY', 'CLIENT_ID')
+spotify_client_secret = config.get('SPOTIFY', 'CLIENT_SECRET')
 
 # 创建数据库连接字符串
 connection_string = f"DRIVER={azure_sql_driver};SERVER={azure_sql_server};DATABASE={azure_sql_database};UID={azure_sql_user};PWD={azure_sql_password}"
+
+# 设置Spotify API客户端
+spotify_credentials = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
+spotify = spotipy.Spotify(client_credentials_manager=spotify_credentials)
 
 # 示例笑话和谜语列表
 jokes = [
@@ -50,7 +58,7 @@ riddles = [
 
 # 定义命令处理函数
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Hi! I'm a Chatbot. I can tell jokes and riddles. Use /joke or /riddle to have fun!")
+    update.message.reply_text("Hi! I'm a Chatbot. I can tell jokes, riddles, and recommend songs from Spotify. Use /joke, /riddle or /song to have fun!")
 
 def joke(update: Update, context: CallbackContext):
     update.message.reply_text(random.choice(jokes))
@@ -59,6 +67,14 @@ def riddle(update: Update, context: CallbackContext):
     selected_riddle = random.choice(riddles)
     update.message.reply_text(selected_riddle["question"])
     update.message.reply_text("Answer: " + selected_riddle["answer"])
+
+def song(update: Update, context: CallbackContext):
+    recommendations = spotify.recommendations(seed_genres=['pop'], limit=1)
+    track = recommendations['tracks'][0]
+    track_name = track['name']
+    track_artist = track['artists'][0]['name']
+    track_url = track['external_urls']['spotify']
+    update.message.reply_text(f"Check out this song: {track_name} by {track_artist}\n{track_url}")
 
 def main():
     # 创建 Updater 和用于处理命令的 CommandHandler
@@ -69,6 +85,7 @@ def main():
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("joke", joke))
     dispatcher.add_handler(CommandHandler("riddle", riddle))
+    dispatcher.add_handler(CommandHandler("song", song))
 
     # 启动 Chatbot
     updater.start_polling()
